71067d29b548b0c8feda5b7c9d9412d394dd804c
Niejun <yinn@tcd.ie>
2021-04-22 13:26:29 +0100

    Revert "DFS algorithm"
    This reverts commit f4c014dd

--- minesweeper/dfs.py
+++ minesweeper/dfs.py
@@ -2,7 +2,6 @@ import logging
 import minesweeper as ms
 import random
 
-
 class DepthFirstSearch(ms.AI):
     """
     To build a new AI, create a new child class of ms.AI
@@ -27,21 +26,21 @@ class DepthFirstSearch(ms.AI):
         x, y = position
         adj = []
         if y < self.height - 1:
-            adj.append((x, y + 1))  # top
+            adj.append((x, y+1)) # top
         if y < self.height - 1 and x < self.width - 1:
-            adj.append((x + 1, y + 1))  # top-right
+            adj.append((x+1, y+1)) # top-right
         if x < self.width - 1:
-            adj.append((x + 1, y))  # right
+            adj.append((x+1, y)) # right
         if x < self.width - 1 and y > 0:
-            adj.append((x + 1, y - 1))  # bottom-right
+            adj.append((x+1, y-1)) # bottom-right
         if y > 0:
-            adj.append((x, y - 1))  # bottom
+            adj.append((x, y-1)) # bottom
         if x > 0 and y > 0:
-            adj.append((x - 1, y - 1))  # bottom-left
+            adj.append((x-1, y-1)) # bottom-left
         if x > 0:
-            adj.append((x - 1, y))  # left
+            adj.append((x-1, y)) # left
         if x > 0 and y < self.height - 1:
-            adj.append((x - 1, y + 1))  # top-left
+            adj.append((x-1, y+1)) # top-left
 
         return adj
 
@@ -63,11 +62,11 @@ class DepthFirstSearch(ms.AI):
 
             for x, y in self.getAdjacents(currPosition):
                 # Just for debug purposes
-                # print(">>> Visiting (", currPosition[0], ",", currPosition[1], ")'s adjacent (", x, ",", y, ")")
-
+                print(">>> Visiting (", currPosition[0],",", currPosition[1], ")'s adjacent (", x,",", y, ")")
+                
                 if (x, y) not in self.exposed_squares:
                     self.positionStack.append((x, y))
-
+            
             return currPosition
 
         # If you need to set a flag, use this method
@@ -93,7 +92,7 @@ class DepthFirstSearch(ms.AI):
         for square in result.new_squares:
             # print("(", square.x, ",", square.y, ")") # Just for debug purposes
             self.exposed_squares.add((square.x, square.y))
-
+    
     @property
     def flags(self):
         """
@@ -103,26 +102,26 @@ class DepthFirstSearch(ms.AI):
 
 
 # This is just a configuration for the logger
-# logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-# print("This will play a single game and then quit.")
-# print("The minesweeper window needs focus to capture a key press.")
-# print()
-#
-# # Number of games to play in a row (useful for testing)
-# num_games = 1
-#
-# # Configuration of the game.
-# # Possible parameters are 'width', 'height', 'num_mines', 'auto_expand_clear_areas'
-# # To place the mines as you want, see file 'minesweeper/minesweeper.py', method '_place_mines'
-# config = ms.GameConfig(num_mines=1, auto_expand_clear_areas=False)
-#
-# # Create an instance of our AI (startPosition has been created by myself, can be random or anything else)
-# ai = DepthFirstSearch((2, 2))
-#
-# # Create a game visualizer - use pause=1 to execute a new move every 1 second (or any other number)
-# viz = ms.PyGameVisualizer(pause='key', next_game_prompt=True)
-#
-# # Run the game(s)
-# result = ms.run_games(config, num_games, ai, viz).pop()
-# print('Game lasted {0} moves'.format(result.num_moves))
+print("This will play a single game and then quit.")
+print("The minesweeper window needs focus to capture a key press.")
+print()
+
+# Number of games to play in a row (useful for testing)
+num_games = 1
+
+# Configuration of the game.
+# Possible parameters are 'width', 'height', 'num_mines', 'auto_expand_clear_areas'
+# To place the mines as you want, see file 'minesweeper/minesweeper.py', method '_place_mines'
+config = ms.GameConfig(num_mines=1, auto_expand_clear_areas=False)
+
+# Create an instance of our AI (startPosition has been created by myself, can be random or anything else)
+ai = DepthFirstSearch((2,2))
+
+# Create a game visualizer - use pause=1 to execute a new move every 1 second (or any other number)
+viz = ms.PyGameVisualizer(pause='key', next_game_prompt=True)
+
+# Run the game(s)
+result = ms.run_games(config, num_games, ai, viz).pop()
+print('Game lasted {0} moves'.format(result.num_moves))

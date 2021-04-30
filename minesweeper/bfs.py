d14a5bb51379707ac5c2596003d4cc01651eea80
Rui <ruxu@tcd.ie>
2021-04-30 10:45:41 +0100

    add main for each agent

--- minesweeper/bfs.py
+++ minesweeper/bfs.py
@@ -150,20 +150,21 @@ print("This will play a single game and then quit.")
 print("The minesweeper window needs focus to capture a key press.")
 print()
 
-# # Number of games to play in a row (useful for testing)
-# num_games = 1
-#
-# # Configuration of the game.
-# # Possible parameters are 'width', 'height', 'num_mines', 'auto_expand_clear_areas'
-# # To place the mines as you want, see file 'minesweeper/minesweeper.py', method '_place_mines'
-# config = ms.GameConfig(width=8, height=8, num_mines=3, auto_expand_clear_areas=False)
-#
-# # Create an instance of our AI (startPosition has been created by myself, can be random or anything else)
-# ai = BreathFirstSearch((3, 3))
-#
-# # Create a game visualizer - use pause=1 to execute a new move every 1 second (or any other number)
-# # viz = ms.PyGameVisualizer(pause=1, next_game_prompt=True)
-#
-# # Run the game(s)
-# result = ms.run_games(config, num_games, ai, None).pop()
-# print('Game lasted {0} moves'.format(result.num_moves))
+if __name__ == '__main__':
+    # Number of games to play in a row (useful for testing)
+    num_games = 1
+
+    # Configuration of the game.
+    # Possible parameters are 'width', 'height', 'num_mines', 'auto_expand_clear_areas'
+    # To place the mines as you want, see file 'minesweeper/minesweeper.py', method '_place_mines'
+    config = ms.GameConfig(width=8, height=8, num_mines=3, auto_expand_clear_areas=False)
+
+    # Create an instance of our AI (startPosition has been created by myself, can be random or anything else)
+    ai = BreathFirstSearch((3, 3))
+
+    # Create a game visualizer - use pause=1 to execute a new move every 1 second (or any other number)
+    viz = ms.PyGameVisualizer(pause="key", next_game_prompt=True)
+
+    # Run the game(s)
+    result = ms.run_games(config, num_games, ai, None).pop()
+    print('Game lasted {0} moves'.format(result.num_moves))
